07910a3e8d031752243a9901a66424a60c5e69bb
Peter J Tshiani <53077580+peterjtshiani@users.noreply.github.com>
2020-02-23 01:29:31 +0200

    Updates

---
+++ precache-manifest.3bd1a7f66b7e63fd46c54841c3df59cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47f390bb660a4bd51e68fe0c4fac60a",
+    "url": "/react-slideshow2/index.html"
+  },
+  {
+    "revision": "52421f134e27bc7a7fb9",
+    "url": "/react-slideshow2/static/css/main.a4dbb8a0.chunk.css"
+  },
+  {
+    "revision": "bdbf1f5b2c4b4b45aa07",
+    "url": "/react-slideshow2/static/js/2.b0d0e8ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-slideshow2/static/js/2.b0d0e8ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52421f134e27bc7a7fb9",
+    "url": "/react-slideshow2/static/js/main.dc6f54cb.chunk.js"
+  },
+  {
+    "revision": "2121ed74517a7112a5a6",
+    "url": "/react-slideshow2/static/js/runtime-main.66561794.js"
+  },
+  {
+    "revision": "92178fab93794343a9f90b368a96a15c",
+    "url": "/react-slideshow2/static/media/IMG_0217.92178fab.jpg"
+  },
+  {
+    "revision": "34093694fbb72b8326b67157ada0a11e",
+    "url": "/react-slideshow2/static/media/alex_140.34093694.JPG"
+  },
+  {
+    "revision": "77e4d30e66eb2a000a5d8a2a0b7494cf",
+    "url": "/react-slideshow2/static/media/alex_69.77e4d30e.JPG"
+  },
+  {
+    "revision": "eedd152028fab866c7871d40b1aa9b1d",
+    "url": "/react-slideshow2/static/media/alex_91.eedd1520.JPG"
+  },
+  {
+    "revision": "398e99805ff768f3a341988e21708f3c",
+    "url": "/react-slideshow2/static/media/wj_11.398e9980.jpg"
+  }
+]);
